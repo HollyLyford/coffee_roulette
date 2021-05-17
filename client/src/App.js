fa4214fce3a6b96c93e0c0fbe7ffe1f0afe97ac0
Holly L <holly.lyford@gmail.com>
2021-05-17 16:49:07 +0100

    MVP complete - routes.js tidied up

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,53 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar'
 import Routes from './components/Routes'
+import names from'./components/initialData';
 
-function App() {
+function App(props) {
+
+  let initialPairs = [
+    { a: 1, b: 2 },
+    { a: 3, b:4},]
+  const[pairs, setPairs] = useState(initialPairs)
+  const[nameList, setNameList] = useState(names)
+  const [nextID, setNextID] = useState(names[names.length-1].id +1)
+
+  function shufflePairs(){
+    let currentIDs = []
+    nameList.forEach((name => currentIDs.push(name.id)))
+    shuffle(currentIDs)
+    setPairs([]);
+    if (currentIDs.length % 2 !== 0){
+        let repeatID = currentIDs[0];
+        currentIDs.push(repeatID);
+    }
+    for (let i = 0; i < currentIDs.length; i+=2){
+        setPairs(newArr => [...newArr, {"a": currentIDs[i], "b": currentIDs[i+1]}])
+    }
+    window.alert("Names have been shuffled")
+  }
+
+  function shuffle(ids) {
+      let m = ids.length, t, i;
+      // While there remain elements to shuffle…
+          while (m) {
+          // Pick a remaining element…
+          i = Math.floor(Math.random() * m--);
+          // And swap it with the current element.
+          t = ids[m];
+          ids[m] = ids[i];
+          ids[i] = t;
+          }
+          //https://bost.ocks.org/mike/shuffle/
+      return ids; 
+  }
+
+  function addPerson(p){
+    p.id = nextID;
+    setNextID(nextID => nextID + 1);
+    setNameList(nameList => [...nameList, p]);
+  }
 
   return (
     <div className="App">
@@ -13,7 +57,12 @@ function App() {
         <h3>👋☕🤝</h3>
       </div>
       {/* <img src="https://static.vecteezy.com/system/resources/thumbnails/000/091/124/small/free-business-meeting-tables-vector-icons.jpg" /> */}
-      <Routes />
+      <Routes 
+        shuffleCb={(ids) => shufflePairs(ids)}
+        addPersonCb={(p) => addPerson(p)}
+        pairs={pairs}
+        nameList={nameList}
+      />
     </div>
   );
   }
--- client/src/components/Routes.js
+++ client/src/components/Routes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 // import AboutView from '../components/AboutView';
@@ -6,53 +6,11 @@ import HomeView from './Home';
 import PairView from './PairView';
 import LogIn from './LogIn';
 import AdminView from './AdminView'
-import names from'./initialData';
 
 function Routes(props) {
 
-    let initialPairs = [
-        { a: 1, b: 2 },
-        { a: 3, b:4},]
-    const[pairs, setPairs] = useState(initialPairs)
-    const[nameList, setNameList] = useState(names)
-    const [nextID, setNextID] = useState(names[names.length-1].id +1)
-
-    function shufflePairs(){
-        let currentIDs = []
-        nameList.forEach((name => currentIDs.push(name.id)))
-        shuffle(currentIDs)
-        setPairs([]);
-        if (currentIDs.length % 2 !== 0){
-            let repeatID = currentIDs[0];
-            currentIDs.push(repeatID);
-        }
-        for (let i = 0; i < currentIDs.length; i+=2){
-            setPairs(newArr => [...newArr, {"a": currentIDs[i], "b": currentIDs[i+1]}])
-        }
-        window.alert("Names have been shuffled")
-      }
-
-
-    function shuffle(ids) {
-        let m = ids.length, t, i;
-        // While there remain elements to shuffle…
-            while (m) {
-            // Pick a remaining element…
-            i = Math.floor(Math.random() * m--);
-            // And swap it with the current element.
-            t = ids[m];
-            ids[m] = ids[i];
-            ids[i] = t;
-            }
-            //https://bost.ocks.org/mike/shuffle/
-        return ids; 
-    }
-    
-    function addPerson(p){
-          p.id = nextID;
-          setNextID(nextID => nextID + 1);
-          setNameList(nameList => [...nameList, p]);
-    }
+    let pairs = props.pairs
+    let nameList = props.nameList
 
     return (
         <Switch>
@@ -60,7 +18,6 @@ function Routes(props) {
                 <HomeView />
             </Route>
     
-            {/* pairs */}
             <Route path="/match">
                 <PairView allPairs={pairs} names={nameList}/>
             </Route>
@@ -72,8 +29,8 @@ function Routes(props) {
             <Route path="/admin">
                 <AdminView 
                     names={nameList} 
-                    shuffleCb={(ids) => shufflePairs(ids)}
-                    submitCb={(p) => addPerson(p)}
+                    shuffleCb={(ids) => props.shuffleCb(ids)}
+                    submitCb={(p) => props.addPersonCb(p)}
                 />
             </Route>
         </Switch>
